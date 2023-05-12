71f0dfb5edbcf5e540ed5fa43ba8927787ee6a6c
sss26 <s.sholto.smith@gmail.com>
2023-05-12 07:42:41 -0700

    Changed InvoiceService to be a prototype rather than the default singleton

--- src/main/java/com/smthasa/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
+++ src/main/java/com/smthasa/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
@@ -1,7 +1,9 @@
 package com.smthasa.myfancypdfinvoices.context;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.smthasa.myfancypdfinvoices.service.InvoiceService;
@@ -11,6 +13,7 @@ import com.smthasa.myfancypdfinvoices.service.UserService;
 public class MyFancyPdfInvoicesApplicationConfiguration {
 
     @Bean
+    @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public UserService userService() {
         return new UserService();
     }
