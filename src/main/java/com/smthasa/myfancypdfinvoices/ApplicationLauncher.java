da976773647d1373ed043503111e7074a3139aa3
sss26 <s.sholto.smith@gmail.com>
2023-05-19 07:25:13 -0700

    Introduced Spring MVC, added basic HelloWorld controller, registered DispatcherServlet with Tomcat to properly set up request dispatching to the controller, and response dispatching from the controller. Minimal use of Spring MVC, whoooo!

--- src/main/java/com/smthasa/myfancypdfinvoices/ApplicationLauncher.java
+++ src/main/java/com/smthasa/myfancypdfinvoices/ApplicationLauncher.java
@@ -2,12 +2,17 @@ package com.smthasa.myfancypdfinvoices;
 
 import java.io.IOException;
 
+import javax.servlet.ServletContext;
+
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.Wrapper;
 import org.apache.catalina.startup.Tomcat;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
-import com.smthasa.myfancypdfinvoices.web.MyFancyPdfInvoicesServlet;
+import com.smthasa.myfancypdfinvoices.context.MyFancyPdfInvoicesApplicationConfiguration;
 
 public class ApplicationLauncher {
 
@@ -20,8 +25,11 @@ public class ApplicationLauncher {
         tomcat.setPort(8080);
         tomcat.getConnector();
 
-        Context ctx = tomcat.addContext("", null);
-        Wrapper servlet = Tomcat.addServlet(ctx, "myFirstServlet", new MyFancyPdfInvoicesServlet());
+        Context tomcatCtx = tomcat.addContext("", null);
+
+        WebApplicationContext appCtx = createApplicationContext(tomcatCtx.getServletContext());
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(appCtx);
+        Wrapper servlet = Tomcat.addServlet(tomcatCtx, "dispatcherServlet", dispatcherServlet);
         servlet.setLoadOnStartup(1);
         servlet.addMapping("/*");
 
@@ -38,6 +46,15 @@ public class ApplicationLauncher {
             }
             System.exit(0);
         }
-
     }
+
+    public static WebApplicationContext createApplicationContext(ServletContext servletContext) {
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(MyFancyPdfInvoicesApplicationConfiguration.class);
+        ctx.setServletContext(servletContext);
+        ctx.refresh();
+        ctx.registerShutdownHook();
+        return ctx;
+    }
+
 }
---
+++ src/main/java/com/smthasa/myfancypdfinvoices/web/MyFirstSpringController.java
@@ -0,0 +1,16 @@
+package com.smthasa.myfancypdfinvoices.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyFirstSpringController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String index() {
+        return "Hello World";
+    }
+
+}
