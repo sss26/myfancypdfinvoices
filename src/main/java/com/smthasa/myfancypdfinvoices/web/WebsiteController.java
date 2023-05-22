b98e0fe9996e511ee22e1d5b3838d90ba16bd1d0
sss26 <s.sholto.smith@gmail.com>
2023-05-22 08:14:04 -0700

    Added login page that takes username and password using form submission. Validation performed against username and password, with informative error message if login fails. Redirect to homepage if login successful. Login currently spoofed to succeed when username == password.

--- src/main/java/com/smthasa/myfancypdfinvoices/web/WebsiteController.java
+++ src/main/java/com/smthasa/myfancypdfinvoices/web/WebsiteController.java
@@ -2,10 +2,17 @@ package com.smthasa.myfancypdfinvoices.web;
 
 import java.util.Date;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.smthasa.myfancypdfinvoices.web.forms.LoginForm;
 
 @Controller
 public class WebsiteController {
@@ -15,5 +22,23 @@ public class WebsiteController {
         model.addAttribute("username", username);
         model.addAttribute("currentDate", new Date());
         return "index.html";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model){
+        model.addAttribute("loginForm", new LoginForm());
+        return "login.html";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute @Valid LoginForm loginForm, BindingResult bindingResult, Model model){
+        if (bindingResult.hasErrors()) {
+            return "login.html";
+        } else if (loginForm.getUsername().equals(loginForm.getPassword())) {
+            return "redirect:/";
+        } else {
+            model.addAttribute("invalidCredentials", "true");
+            return "login.html";
+        }
     }
 }
